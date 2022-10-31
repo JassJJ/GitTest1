ec7e42da780c03fd33ff11a6ae6703d8be4df3a3
jasje <Jasje2003@gmail.com>
2022-10-31 10:57:30 +0100

    add nothing

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("    /\\_____/\\     ");
+        System.out.println("   / .     . \\     ");
+        System.out.println("  {     *     }    ");
+        System.out.println("   \\_________/   ");
+        System.out.println("   /         \\      ");
+        System.out.println("  /           \\    ");
+
+    }
+        }
